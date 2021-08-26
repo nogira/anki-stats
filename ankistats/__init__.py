ae2d67fd6c0070a09922b4403f7afa6b726c30e3
nogira <83847986+nogira@users.noreply.github.com>
2021-08-26 14:41:14 +1000

    refactor code

--- ankistats/__init__.py
+++ ankistats/__init__.py
@@ -2,16 +2,10 @@
 __version__ = "0.2.2"
 
 
-from os.path import exists, abspath
-from requests import get
-csv_path = abspath(__file__+'/../unigram_freq.csv')
-if exists(csv_path):
-    pass
-else:
-    with open(csv_path, 'w') as f:
-        url = 'https://raw.githubusercontent.com/nogira/anki-stats/main/unigram_freq.csv'
-        f.write(get(url).text)
-        del url
-del csv_path
+from .tables import *
+from .plots import *
+from .stats import *
+from .simulation import *
 
-from .db_class import *
+def db_path(path):
+    full_db.path = path
